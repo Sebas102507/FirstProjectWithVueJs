1d62d7fced0226317bf14b46fc8b18fb72e4afb1
Juan Sebastian Vargas Torres <libros123bmw@gmail.com>
2022-06-05 07:33:13 -0500

    Computed and Watchers

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ new Vue({
   data(){
     return {
       name: "Ethereum",
+      symbol: "ETH",
       image: "https://www.spectre.ai/assets/images/assets/ETH-logo.png?v=2.13",
       changePercent: 10,
       prices: [8400, 7900, 8200, 9000, 9400, 10000, 10200],
@@ -21,6 +22,22 @@ new Vue({
     }
   },
 
+
+  computed:{
+
+    title(){
+      return `${this.name} - ${this.symbol}`
+    }
+
+  },
+
+  watch:{
+
+    showPrices(newVal, oldVal){
+      console.log(newVal,oldVal)
+    }
+
+  },
 
   methods : {
 
