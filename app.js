8e9982a38af90edaf04b11b6fbcbcf6224fe736a
Juan Sebastian Vargas Torres <libros123bmw@gmail.com>
2022-06-04 22:29:28 -0500

    First Commit of the project

---
+++ app.js
@@ -0,0 +1,11 @@
+new Vue({
+  el:'#app',
+  data(){
+    return {
+      name: "Ethereum",
+      image: "https://www.spectre.ai/assets/images/assets/ETH-logo.png?v=2.13",
+      changePercent: 10,
+      prices: [8400, 7900, 8200, 9000, 9400, 10000, 10200]
+    }
+  }
+})
